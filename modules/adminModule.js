1c891963dd96bb041c05ad68885a29f98d3c1216
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2020-08-13 11:52:21 +0200

    update addMeal

--- modules/adminModule.js
+++ modules/adminModule.js
@@ -1,6 +1,51 @@
 const connect = require("../models/connectionFn");
-const ADDMENU = require("../models/menuSchema");
+const MENUSCHEMA = require("../models/menuSchema");
+const fs = require("fs");
 
-function addMeal(title, discription, number, price, category, img) {}
+function addMeal(
+  title,
+  description,
+  number,
+  price,
+  category,
+  img,
+  restaurantId
+) {
+  return new Promise((resolve, reject) => {
+    connect()
+      .then(() => {
+        let date = Date.now();
+        let imgEnd = img.name.substr(img.name.lastIndexOf("."));
+        let newImgName =
+          title.trim().replace(/ /g, "_") +
+          "_" +
+          restaurantId +
+          "_" +
+          date +
+          "_" +
+          imgEnd;
+        let newImgNameDb = "/upload/" + newImgName;
+        img.mv("./public/uploads/" + newImgName);
+        const newMeal = new MENUSCHEMA({
+          title: title,
+          description: description,
+          number: number,
+          price: price,
+          category: category,
+          img: newImgNameDb,
+          restaurantId: restaurantId,
+        });
+        newMeal
+          .save()
+          .then(() => {
+            resolve(1);
+          })
+          .catch(() => {
+            reject(new Error("can not add meal!"));
+          });
+      })
+      .catch((err) => console.log(err));
+  });
+}
 
 module.exports = { addMeal };
