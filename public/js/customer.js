847795e46217d89124ea6d4eddf2d5414a8c877f
full <shirinvalizadeh6004@gmail.com>
2020-08-16 12:06:32 +0200

    admin_deleteMeal_finish

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,1 +1,19 @@
 
+   //-----1------- if we have error will call this function---
+   function showModal(error, title, content) {
+    const modal = document.querySelector('#resultModal')
+    const modal_header = modal.querySelector('.modal-header')
+    const modal_body = modal.querySelector('.modal-body')
+
+    if (error) {
+        modal_header.classList.add('bg-danger')
+        modal_header.classList.remove('bg-success')
+    } else {
+        modal_header.classList.remove('bg-danger')
+        modal_header.classList.add('bg-success')
+    }
+    modal_header.innerHTML = title
+    modal_body.innerHTML = '<p>' + content + '</p>'
+    $('#resultModal').modal('show')
+}
+
